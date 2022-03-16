990723e5754426650376c206ee88a792bb36c587
SBobi <s.bobadilla@uniandes.edu.co>
2022-03-16 17:58:40 -0500

    Ya-Ha

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Tamaño de la pantalla
 let width = window.screen.availWidth;
-
+ 
 // Path de los datos
 const url = "https://gist.githubusercontent.com/josejbocanegra/9a28c356416badb8f9173daf36d1460b/raw/5ea84b9d43ff494fcbf5c5186544a18b42812f09/restaurant.json";
 const test = document.getElementById("emerg");
